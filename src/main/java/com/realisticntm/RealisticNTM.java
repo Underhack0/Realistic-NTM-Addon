918dfb2045977dad7d312cab716b22b531c6152d
Gerzo <dimmka2010@gmail.com>
2026-08-26 19:06:32 +0300

    fix-for-cursed-addon

--- src/main/java/com/realisticntm/RealisticNTM.java
+++ src/main/java/com/realisticntm/RealisticNTM.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.realisticntm;
 
 import net.minecraftforge.fml.common.Mod;
@@ -29,35 +28,3 @@ public class RealisticNTM {
         RadiationOverrideApplier.apply();
     }
 }
-=======
-package com.realisticntm;
-
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.event.FMLLoadCompleteEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-
-import org.apache.logging.log4j.LogManager;
-
-import com.realisticntm.config.RadiationConfigRTM;
-import com.realisticntm.radiation.RadiationOverrideApplier;
-
-@Mod(modid = Tags.MODID,
-     name = Tags.MODNAME,
-     version = Tags.VERSION,
-     dependencies = "required-after:mixinbooter;after:hbm",
-     acceptedMinecraftVersions = "[1.12,1.13)")
-public class RealisticNTM {
-
-    public static final org.apache.logging.log4j.Logger LOGGER = LogManager.getLogger(Tags.MODNAME);
-
-    @Mod.EventHandler
-    public void onPreInit(FMLPreInitializationEvent event) {
-        RadiationConfigRTM.init(event.getSuggestedConfigurationFile());
-    }
-
-    @Mod.EventHandler
-    public void onLoadComplete(FMLLoadCompleteEvent event) {
-        RadiationOverrideApplier.apply();
-    }
-}
->>>>>>> 678bf43fba2c86bf348971819cc46252882eb0c2
--- src/main/java/com/realisticntm/mixin/MixinHazardTypeRadiation.java
+++ src/main/java/com/realisticntm/mixin/MixinHazardTypeRadiation.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.realisticntm.mixin;
 
 import java.util.List;
@@ -56,47 +55,3 @@ public class MixinHazardTypeRadiation {
         }
     }
 }
-=======
-package com.realisticntm.mixin;
-
-import java.util.List;
-
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.ItemStack;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
-
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
-
-import com.hbm.hazard.modifier.IHazardModifier;
-import com.hbm.hazard.type.HazardTypeRadiation;
-import com.hbm.util.I18nUtil;
-import com.realisticntm.config.RadiationConfigRTM;
-import com.realisticntm.radiation.BqFormat;
-
-@Mixin(value = HazardTypeRadiation.class, remap = false)
-public class MixinHazardTypeRadiation {
-
-    /**
-     * @author Realistic NTM
-     * @reason Display item activity in becquerels instead of RAD/s.
-     */
-    @Overwrite(remap = false)
-    @SideOnly(Side.CLIENT)
-    public void addHazardInformation(EntityPlayer player, List<String> list, double level, ItemStack stack,
-                                     List<IHazardModifier> modifiers) {
-        level = IHazardModifier.evalAllModifiers(stack, player, level, modifiers);
-        if (level == 0) return;
-
-        double bqPerItem = level * RadiationConfigRTM.bqPerRadPerSecond;
-
-        list.add("§a[" + I18nUtil.resolveKey("trait.radioactive") + "]");
-        list.add(" §e" + BqFormat.format(bqPerItem));
-
-        if (stack.getCount() > 1) {
-            list.add(" §e" + I18nUtil.resolveKey("desc.stack") + " " + BqFormat.format(bqPerItem * stack.getCount()));
-        }
-    }
-}
->>>>>>> 678bf43fba2c86bf348971819cc46252882eb0c2
